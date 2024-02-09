e14bdc03289afc7c96525c1e614ae54955ffd67e
DaniDrury <145943483+DaniDrury@users.noreply.github.com>
2024-02-08 18:13:11 -0800

    commit after outage

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,27 @@
-// mysql2 promise package
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
-let db;
-
-// Connect to database
-try {
-db = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'mOr3StrOngPw$1!',
-  database: 'employee_db'
+const db = await mysql.createConnection({
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: 3306
 });
-} catch(err) {
-  console.log(err);
-};
 
 export default db;
+
+// import mysql from "mysql2/promise";
+
+// let db;
+
+// try {
+//   const { DB_DATABASE, DB_USER, DB_PASSWORD } = process.env;
+
+//   db = await mysql.createConnection(
+//     `mysql://${DB_USER}:${DB_PASSWORD}@localhost:3306/${DB_DATABASE}`
+//   );
+// } catch (error) {
+//   console.log(error);
+// }
+
+// export default db;
